bbcc22f0caada8ae716d64e149f2c34ce8769fe0
Hendrik A <lightjavacode@gmail.com>
2023-05-26 22:58:02 +0200

    -

--- AshObjects/source/AshBufferPlug.cpp
+++ AshObjects/source/AshBufferPlug.cpp
@@ -12,16 +12,27 @@ namespace ash::objects
 
     static constexpr ash::AshSize smAshBufferPlugBufferSizeLimit = 0x1fff;
 
-    AshBufferPlug::AshBufferPlug()
+    AshBufferPlug::AshBufferPlug():
+        buffer(nullptr),
+        bufferEmpty(false),
+        bufferSizeLimit(smAshBufferPlugBufferSizeLimit)
     {}
 
     AshBufferPlug::AshBufferPlug(ash::AshBuffer* Buffer):
-        buffer(Buffer),
+        buffer(nullptr),
+        bufferEmpty(false),
         bufferSizeLimit(smAshBufferPlugBufferSizeLimit)
     {}
 
     bool AshBufferPlug::Import(ash::AshStream* Stream)
     {
+        if(this->buffer != nullptr)
+        {
+            this->buffer->ReleaseMemory();
+            delete this->buffer;
+            this->buffer = nullptr;
+        }
+
         AshSize bufferSize = Stream->Read<AshSize>();
 
         if(bufferSize > bufferSizeLimit)
@@ -45,13 +56,21 @@ namespace ash::objects
 
     bool AshBufferPlug::Export(ash::AshStream* Stream)
     {
+        if(this->buffer == nullptr)
+        {
+            // By-pass
+            Stream->Write<AshSize>(0);
+            return true;
+        }
+
         Stream->Write<AshSize>(this->buffer->GetSize());
 
         if(this->buffer->GetSize() <= 0)
         {
+            this->bufferEmpty = true;
             return Stream->HasErrorOccurred() == false;
         }
-        
+
         Stream->WriteRawFromPointer(this->buffer->GetPointer(), this->buffer->GetSize());
 
         return Stream->HasErrorOccurred() == false;
